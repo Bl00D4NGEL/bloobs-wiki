ed15d2aa5cafd1d44722d4e9782f895324606699
Bl00D4NGEL <github@d-peters.com>
2026-06-20 01:36:51 +0200

    feat: add Beastmastery, SoulBinding and Homesteading formatter

--- challenges.py
+++ challenges.py
@@ -23,21 +23,21 @@ def read_challenges_csv(filename: str) -> list[Challenge]:
     challenges = []
     
     # Read the file with UTF-8 encoding and handle BOM
-    with open(filename, 'r', encoding='utf-8-sig', newline='') as file:
+    with open(filename, "r", encoding="utf-8-sig", newline="") as file:
         reader = csv.DictReader(file)
         
         for row in reader:            
             challenges.append(Challenge(
-                category=row['Category'],
-                challenge_name=row['Challenge Name'],
-                challenge_id=row['Challenge ID'],
-                challenge_type=row['Type'],
-                skill=row['Skill'],
-                level_required=int(row['Level Required']),
-                tier=int(row['Tier']),
-                requirement_amount=int(row['Requirement Amount']),
-                challenge_points=int(row['Challenge Points']),
-                repeatable=row['Repeatable'],
+                category=row["Category"],
+                challenge_name=row["Challenge Name"],
+                challenge_id=row["Challenge ID"],
+                challenge_type=row["Type"],
+                skill=row["Skill"],
+                level_required=int(row["Level Required"]),
+                tier=int(row["Tier"]),
+                requirement_amount=int(row["Requirement Amount"]),
+                challenge_points=int(row["Challenge Points"]),
+                repeatable=row["Repeatable"],
             ))
     
     return challenges
@@ -82,6 +82,9 @@ class SkillFormatter(ABC):
     def supported_categories(self) -> list[str]:
         pass
 
+    def format_category(self, category: str) -> str:
+        return category
+
     @abstractmethod
     def get_description(self, category: str) -> Optional[str]:
         pass
@@ -99,22 +102,30 @@ class ResourceFormatter(SkillFormatter):
     _resource_names: dict[str, str]
     _resource_image_dict: dict[str, str]
     _resource_name_overrides: dict[str, str]
+    _category_name_overrides: dict[str, str]
 
     def __init__(
         self,
         descriptions: dict[str, str],
         resource_names: dict[str, str],
         resource_image_dict: dict[str, str] = {},
-        resource_name_overrides: dict[str, str] = {}
+        resource_name_overrides: dict[str, str] = {},
+        category_name_overrides: dict[str, str] = {}
     ) -> None:
         super().__init__()
         self._descriptions = descriptions
         self._resource_names = resource_names
         self._resource_image_dict = resource_image_dict
         self._resource_name_overrides = resource_name_overrides
+        self._category_name_overrides = category_name_overrides
 
     def supported_categories(self) -> list[str]:
         return list(self._descriptions.keys())
+
+    def format_category(self, category: str) -> str:
+        if category in self._category_name_overrides:
+            return self._category_name_overrides[category]
+        return super().format_category(category)
 
     def get_description(self, category: str) -> str | None:
         return self._descriptions[category]
@@ -184,14 +195,14 @@ class SimpleFormatter(SkillFormatter):
 
 class WoodcuttingFormatter(SkillFormatter):
     def supported_categories(self) -> list[str]:
-        return ['Trees']
+        return ["Trees"]
 
     def get_description(self, category: str) -> str | None:
         return "These challenges are progressed by successfully chopping the log from the tree listed in the challenge."
 
     def get_header_cells(self, category: str, challenges_by_id: dict[str, list[Challenge]]) -> list[str]:
         return ["Required Level", "Tree", "Log", "Tier 1", "Tier 2", "Tier 3", "Tier 4", "Tier 5", "Tier 6", "Tier 7", "Tier 8", "Tier 9", "Tier 10", "Repeatable"]
-    
+
     def get_table_row_cells(self, category: str, challenges_by_id: dict[str, list[Challenge]]) -> list[list[str]]:
         rows = []
 
@@ -227,6 +238,86 @@ class WoodcuttingFormatter(SkillFormatter):
                 cells.append("???")
 
             cells.append(format_image_cell(log_name))
+
+            for challenge in by_id:
+                cells.append(f"{challenge.requirement_amount} ({challenge.challenge_points})")
+
+            if by_id[0].repeatable:
+                cells.append("Yes")
+            else:
+                cells.append("No")
+
+            rows.append(cells)
+
+        return rows
+
+class HomesteadingFormatter(SkillFormatter):
+    def supported_categories(self) -> list[str]:
+        return ["Crops", "Fishing", "Foraging", "Mining", "Woodcutting"]
+
+    def get_description(self, category: str) -> str | None:
+        descriptions = {
+            "Crops": "These challenges are progressed by successfully growing the crop listed in the challenge.",
+            "Fishing": "These challenges are progressed by successfully growing the fish listed in the challenge.",
+            "Foraging": "These challenges are progressed by successfully growing the item listed in the challenge.",
+            "Mining": "These challenges are progressed by successfully growing the ore listed in the challenge.",
+            "Woodcutting": "These challenges are progressed by successfully growing the tree listed in the challenge.",
+        }
+
+        if category in descriptions:
+            return descriptions[category]
+        
+        return None
+
+    def get_header_cells(self, category: str, challenges_by_id: dict[str, list[Challenge]]) -> list[str]:
+        resource_names = {
+                "Crops": "Crop",
+                "Fishing": "Fish",
+                "Foraging": "Item",
+                "Mining": "Ore",
+                "Woodcutting": "Tree",
+        }
+        if category in resource_names:
+            return ["Required Level", resource_names[category], "Tier 1", "Tier 2", "Tier 3", "Tier 4", "Tier 5", "Tier 6", "Tier 7", "Tier 8", "Tier 9", "Tier 10", "Repeatable"]
+
+        return []
+
+    def get_table_row_cells(self, category: str, challenges_by_id: dict[str, list[Challenge]]) -> list[list[str]]:
+        rows = []
+
+        challenge_name_overrides = {
+            "Bloobberry": "Bloobberries",
+            "Red Bloobberry": "Red Bloobberries",
+            "Orange Bloobberry": "Orange Bloobberries",
+            "Yellow Bloobberry": "Yellow Bloobberries",
+            "White Bloobberry": "White Bloobberries",
+            "Purple Bloobberry": "Purple Bloobberries",
+            "Grey Bloobberry": "Grey Bloobberries",
+            "Teal Bloobberry": "Teal Bloobberries",
+            "Frosted Bloobberry": "Frosted Bloobberries",
+        }
+
+        image_overrides = {
+            "Imbued Crystal": "Imbued Shards",
+            "Common Gem Rock": "Common Gem Rock Node",
+            "Uncommon Gem Rock": "Uncommon Gem Rock Node",
+            "Rare Gem Rock": "Rare Gem Rock Node",
+        }
+
+        for by_id in challenges_by_id.values():
+            cells = [f"Level {by_id[0].level_required}"]
+            challenge_name = by_id[0].challenge_name
+            if challenge_name in challenge_name_overrides:
+                challenge_name = challenge_name_overrides[challenge_name]
+
+            match category:
+                case "Crops" | "Fishing" | "Foraging":
+                    cells.append(format_image_cell(challenge_name))
+                case "Mining" | "Woodcutting":
+                    if challenge_name in image_overrides:
+                        cells.append(format_image_cell(challenge_name, image_overrides[challenge_name], False))
+                    else:
+                        cells.append(format_image_cell(challenge_name, None, False))
 
             for challenge in by_id:
                 cells.append(f"{challenge.requirement_amount} ({challenge.challenge_points})")
@@ -336,12 +427,12 @@ def print_challenges_for_skill(challenges: list[Challenge], skill: str, formatte
 
         description = formatter.get_description(category)
         header_cells = formatter.get_header_cells(category, grouped_by_id)
-        out += f'''
-==== {category} ====
+        out += f"""
+==== {formatter.format_category(category)} ====
 {description}
 {{| class="mw-collapsible mw-collapsed wikitable"
 |-
-'''
+"""
         out += "! " + "!! ".join([cell + " " for cell in header_cells]) + "\n"
         out += "|-\n"
         for cells in formatter.get_table_row_cells(category, grouped_by_id):
@@ -363,7 +454,7 @@ def print_challenge(challenge: Challenge) -> None:
     print(f"Repeatable: {challenge.repeatable}")
 
 def main():
-    filename = 'challenges.csv'
+    filename = "challenges.csv"
     
     challenges: list[Challenge] = []
     try:
@@ -379,18 +470,18 @@ def main():
         "Woodcutting": WoodcuttingFormatter,
         "Cooking": lambda: ResourceFormatter(
             {
-                'Fish': "These challenges are progressed by successfully cooking the fish listed in the challenge.",
-                'Meat': "These challenges are progressed by successfully cooking the meat listed in the challenge.",
-                'Other': "These challenges are progressed by successfully cooking the food listed in the challenge.",
+                "Fish": "These challenges are progressed by successfully cooking the fish listed in the challenge.",
+                "Meat": "These challenges are progressed by successfully cooking the meat listed in the challenge.",
+                "Other": "These challenges are progressed by successfully cooking the food listed in the challenge.",
             },
             {
-                'Fish': "Fish",
-                'Meat': "Meat",
-                'Other': "Food",
+                "Fish": "Fish",
+                "Meat": "Meat",
+                "Other": "Food",
             },
             {
                 "Chicken": "Chicken (Food)",
-            }
+            },
         ),
         "Dexterity": DexterityFormatter,
         "Mining": lambda: ResourceFormatter({"Ores": "These challenges are progressed by successfully mining the ore listed in the challenge."}, {"Ores": "Ore"}),
@@ -414,7 +505,7 @@ def main():
                 "Weapons": "Weapon",
                 "Armour": "Armour",
                 "Other": "Other",
-            }
+            },
         ),
         "Herbology": lambda: ResourceFormatter(
             {
@@ -430,7 +521,7 @@ def main():
                 "Flask Of Water": "Flask of Water",
                 "Bottle Of Water": "Bottle of Water",
                 "Decanter Of Water": "Decanter of Water",
-            }
+            },
         ),
         "Fishing": lambda: ResourceFormatter({"Fish": "These challenges are progressed by successfully fishing the fish listed in the challenge."}, {"Fish": "Potion"}),
         "Crafting": lambda: ResourceFormatter(
@@ -458,7 +549,7 @@ def main():
                 "Cape Of Windmark": "Cape of Windmark",
                 "Cape Of Mistborn": "Cape of Mistborn",
                 "Cape Of Ironfang": "Cape of Ironfang",
-            }
+            },
         ),
         "BowCrafting": lambda: ResourceFormatter(
             {
@@ -470,7 +561,7 @@ def main():
                 "Arrows": "Arrow",
                 "Bows": "Bow",
                 "Shafts": "Shaft",
-            }
+            },
         ),
         "Magic": lambda: SimpleFormatter({"Accuracy": "These challenges are progressed by successfully hitting an enemy with a magic attack.", "Damage": "These challenges are progressed by successfully dealing damage to enemies with a magic attack.", "Teleports": "These challenges are progressed by teleporting to the location listed in the challenge."}),
         "Imbuing": lambda: ResourceFormatter(
@@ -486,7 +577,7 @@ def main():
             },
             resource_name_overrides={
                 "Random Tier 0 Slate": "Imbued Omni Slate",
-            }
+            },
         ),
         "Firemaking": lambda: ResourceFormatter(
             {
@@ -510,7 +601,7 @@ def main():
             resource_name_overrides={
                 "Pile Of Bones": "Pile of Bones",
                 "Scorched Pile Of Bones": "Scorched Pile of Bones",
-            }
+            },
         ),
         "Thieving": lambda: ResourceFormatter(
             {
@@ -526,7 +617,7 @@ def main():
             resource_name_overrides={
                 "LumberStall": "Lumber Stall",
                 "Jeweled Tomb Chest": "Jeweled Tomb",
-            }
+            },
         ),
         "Tracking": lambda: ResourceFormatter(
             {
@@ -537,8 +628,81 @@ def main():
             },
             resource_name_overrides={
                 "ElderGlow Spider": "Elderglow Spider",
-            }
+            },
         ),
+        "Beastmastery": lambda: ResourceFormatter(
+            {
+                "Enemy": "These challenges are progressed by successfully killing the enemy listed in the challenge.",
+                "Enemy_Superior": "These challenges are progressed by successfully killing the enemy listed in the challenge.",
+                "Enemy_Golden": "These challenges are progressed by successfully killing the enemy listed in the challenge.",
+                "Enemy_Corrupted": "These challenges are progressed by successfully killing the enemy listed in the challenge.",
+                "Boss": "These challenges are progressed by successfully killing the boss listed in the challenge.",
+                "Boss_Superior": "These challenges are progressed by successfully killing the boss listed in the challenge.",
+                "Boss_Golden": "These challenges are progressed by successfully killing the boss listed in the challenge.",
+                "Boss_Corrupted": "These challenges are progressed by successfully killing the boss listed in the challenge.",
+            },
+            {
+                "Enemy": "Enemy",
+                "Enemy_Superior": "Enemy",
+                "Enemy_Golden": "Enemy",
+                "Enemy_Corrupted": "Enemy",
+                "Boss": "Boss",
+                "Boss_Superior": "Boss",
+                "Boss_Golden": "Boss",
+                "Boss_Corrupted": "Boss",
+            },
+            {
+                "Banana Skirmisher": "Banana Skirmisher Soul",
+                "Graveheart The Revenant Overlord": "Graveheart"
+            },
+            resource_name_overrides={
+                "Chickens": "Chicken (Monster)",
+                "Cows": "Cow",
+                "Crabs": "Crab",
+                "Goblins": "Goblin",
+                "Bats": "Bat",
+                "Goats": "Goat",
+                "Hobgoblins": "Hobgoblin",
+                "Pigs": "Pig",
+                "Sheeps": "Sheep",
+                "Wolves": "Wolf",
+                "Goblin Wolf Riders": "Goblin Wolf Rider",
+                "Hobgoblin Brawlers": "Hobgoblin Brawler",
+                "Boars": "Boar",
+                "Gnolls": "Gnoll",
+                "Skittering Hands": "Skittering Hand",
+                "Toxic Hounds": "Toxic Hound",
+                "Dismembered Crawlers": "Dismembered Crawler",
+                "Skeletons": "Skeleton",
+                "Black Wolves": "Black Wolf",
+                "Carcass Feeders": "Carcass Feeder",
+                "Zombies": "Zombie",
+                "Dark Crabs": "Dark Crab",
+                "Aqua Drakes": "Aqua Drake",
+                "Revenants": "Revenant",
+                "Skeletal Archers": "Skeletal Archer",
+                "Bound Cadavers": "Bound Cadaver",
+                "Unraveling Crawlers": "Unraveling Crawler",
+                "Ancient Fighters": "Ancient Fighter",
+                "Bone Golems": "Bone Golem",
+                "Bony Soldiers": "Bony Soldier",
+                "Juvenile Entropic Dragons": "Juvenile Entropic Dragon",
+                "Toxic Wolves": "Toxic Wolf",
+                "Ghastly Eyes": "Ghastly Eye",
+                "Twinfang the Blight Bringer": "Twinfang The Blight Bringer",
+                "Graveheart": "Graveheart The Revenant Overlord",
+            },
+            category_name_overrides={
+                "Enemy_Superior": "Enemy (Superior)",
+                "Enemy_Golden": "Enemy (Golden)",
+                "Enemy_Corrupted": "Enemy (Corrupted)",
+                "Boss_Superior": "Boss (Superior)",
+                "Boss_Golden": "Boss (Golden)",
+                "Boss_Corrupted": "Boss (Corrupted)",
+            },
+        ),
+        "SoulBinding": lambda: SimpleFormatter({"Auto Bank": "These challenges are progressed by automatically banking the amount of items listed in the challenge.", "Auto Sell": "These challenges are progressed by automatically selling items worth the amount of items listed in the challenge."}),
+        "Homesteading": HomesteadingFormatter,
     }
 
     for [supported_skill, formatter] in supported_skills.items():
